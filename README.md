7dc0f6f7b0b78fc4dcd6cc7e50e2cdfb24a80938
cpardotortosa <cpardo@imayhem.com>
2019-11-12 22:02:40 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dired-hide-permissions
-Emacs Minor mode to hide the permissions block (rwxrwxrwx) on dired buffers
+Emacs Minor mode to hide the permissions block (rwxrwxrwx) on dired buffers.
+
+Activate or deactivate in current dired buffer with `M-x dired-hide-permissions-mode`.
+
+To activate on any new dired buffer, place a call to `(dired-hide-permissions-mode-always)` on your init file.
+
+
---
+++ dired-hide-permissions.el
@@ -0,0 +1,28 @@
+;;; dired-hide-permissions.el --- Select windows by numbers.
+
+(define-minor-mode dired-hide-permissions-mode
+  "Toggle visibility of the permission flags in current Direct buffer."
+  nil nil nil
+  (unless (derived-mode-p 'dired-mode)
+    (error "Not a Dired buffer"))
+  (dired-hide-permissions-maybe))
+
+(defun dired-hide-permissions-maybe()
+  (let ((inhibit-read-only t))
+    (save-excursion
+      (goto-char (point-min))
+      (while (< (point) (point-max))
+        (if (search-forward-regexp dired-re-perms (line-end-position) t)
+            (if dired-hide-permissions-mode
+                (put-text-property (match-beginning 0) (match-end 0) 'display "")
+              (remove-text-properties (match-beginning 0) (match-end 0) '(display nil))))
+        (forward-line 1)))))
+
+(defun dired-hide-permissions-mode-always()
+  (add-hook 'dired-mode-hook
+            'dired-hide-permissions-mode))
+
+(add-hook
+ 'dired-after-readin-hook
+ 'dired-hide-permissions-maybe t)
+          
