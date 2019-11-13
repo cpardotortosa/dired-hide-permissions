a60087d1a7b1d96d18c7f59d38c61b95f3fb3d80
cpardotortosa <cpardo@imayhem.com>
2019-11-13 18:20:37 +0100

    Documentation strings

--- dired-hide-permissions.el
+++ dired-hide-permissions.el
@@ -1,13 +1,14 @@
 ;;; dired-hide-permissions.el --- Hides permissions block in dired buffers.
 
 (define-minor-mode dired-hide-permissions-mode
-  "Toggle visibility of the permission flags in current Direct buffer."
+  "Toggle visibility of the permission flags in current `dired' buffer."
   nil nil nil
   (unless (derived-mode-p 'dired-mode)
     (error "Not a Dired buffer"))
   (dired-hide-permissions-maybe))
 
 (defun dired-hide-permissions-maybe()
+  "Hides or unhides the permissions block in current `dired' buffer."
   (let ((inhibit-read-only t))
     (save-excursion
       (goto-char (point-min))
@@ -19,11 +20,13 @@
         (forward-line 1)))))
 
 (defun dired-hide-permissions-mode-always()
+  "Set hooks to always activate `dired-hide-permissions-mode' on new dired buffers."
   (interactive)
   (add-hook 'dired-mode-hook
             'dired-hide-permissions-mode))
 
 (defun dired-hide-permissions-mode-never()
+  "Unset hooks set by `dired-hide-permissions-mode-always'"
   (interactive)
   (remove-hook 'dired-mode-hook
             'dired-hide-permissions-mode))
